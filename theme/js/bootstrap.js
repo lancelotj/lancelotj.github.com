7fef8837179eded84750c1e37f5bffd29a705f28
Lance Jian <lancelotj100@gmail.com>
2013-08-25 12:08:56 -0700

    fixed drop down

--- theme/js/bootstrap.js
+++ theme/js/bootstrap.js
@@ -685,7 +685,7 @@
       clearMenus()
 
       if (!isActive) {
-        if ('ontouchstart' in document.documentElement) {
+        if ('ontouchstart' in document.documentElement && !$parent.closest('.navbar-nav').length) {
           // if mobile we we use a backdrop because click events don't delegate
           $('<div class="dropdown-backdrop"/>').insertBefore($(this)).on('click', clearMenus)
         }
